6d3a1f0b8cd0476f18d59aae9346ece37aa14602
Kareem El-Faramawi <kareem02@gmail.com>
2016-06-02 10:40:25 -0400

    Add basic arguments

--- mc-sema/bin_descend/get_cfg.py
+++ mc-sema/bin_descend/get_cfg.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
 import binaryninja
+import argparse
+import sys
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--entry-symbol', nargs='*',
+                        help='Symbols(s) to start disassembling from')
+
+    parser.add_argument('-o', '--output',
+                        type=argparse.FileType('wb'),
+                        default=None,
+                        help='The output control flow graph recovered from this file')
+
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Enable verbose debugging mode')
+
+    parser.add_argument('file', help='Binary to recover control flow graph from')
+
+    args = parser.parse_args(sys.argv[1:])
 
 
 if __name__ == '__main__':
